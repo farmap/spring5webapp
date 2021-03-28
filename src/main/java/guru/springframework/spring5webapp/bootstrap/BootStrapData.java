628a39cb300a2bbbd5e101ade8c180138e280dd1
Phil <phil@local.com>
2021-03-28 11:36:02 +0200

    add publisher

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -5,21 +5,24 @@ import org.springframework.stereotype.Component;
 
 import guru.springframework.spring5webapp.domain.Author;
 import guru.springframework.spring5webapp.domain.Book;
+import guru.springframework.spring5webapp.domain.Publisher;
 import guru.springframework.spring5webapp.repositories.AuthorRepository;
 import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
 
 @Component
 public class BootStrapData implements CommandLineRunner {
 	
 	private final AuthorRepository authorRepository;
 	private final BookRepository bookRepository;
+	private final PublisherRepository publisherRepository;
 	
 	
 
-	public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
-		super();
+	public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
 		this.authorRepository = authorRepository;
 		this.bookRepository = bookRepository;
+		this.publisherRepository = publisherRepository;
 	}
 
 
@@ -30,6 +33,10 @@ public class BootStrapData implements CommandLineRunner {
 		Author julie = new Author("Julie", "Stein");
 		Book dune = new Book("dune", "22333");
 		Book sade = new Book("Justine", "666");
+		Publisher roundHouse = new Publisher("Round House", "5 Rolo Road", "New York", "USA");
+		publisherRepository.save(roundHouse);
+		Publisher gallimard = new Publisher("Gallimard", "4 Rue de Turenne", "Paris", "France");
+		publisherRepository.save(gallimard);
 		phil.getBooks().add(dune);
 		dune.getAuthors().add(phil);
 		julie.getBooks().add(sade);
@@ -39,6 +46,9 @@ public class BootStrapData implements CommandLineRunner {
 		bookRepository.save(dune);
 		authorRepository.save(julie);
 		bookRepository.save(sade);
+		for (Publisher publisher: publisherRepository.findAll()) {
+			System.out.println(publisher);
+		}
 		System.out.println("Number of Books: " + bookRepository.count());
 		
 	}
---
+++ src/main/java/guru/springframework/spring5webapp/domain/Publisher.java
@@ -0,0 +1,107 @@
+package guru.springframework.spring5webapp.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Publisher {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String name;
+	private String address;
+	private String city;
+	private String country;
+	
+	
+	public Publisher() {
+		
+	}
+	
+	public Publisher(String name, String address, String city, String country) {
+		this.name = name;
+		this.address = address;
+		this.city = city;
+		this.country = country;
+	}
+	
+	
+
+	@Override
+	public String toString() {
+		return "Publisher [id=" + id + ", name=" + name + ", address=" + address + ", city=" + city + ", country="
+				+ country + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return ((id == null) ? 0 : id.hashCode());
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Publisher other = (Publisher) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	
+	
+	
+	
+	
+	
+
+}
